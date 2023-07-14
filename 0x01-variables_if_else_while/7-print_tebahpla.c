5cb339e201fb090adbf862bec0b9b493dc774436
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-07-14 20:09:53 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,10 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
+int main(void) 
     int i;
-    
     for (i = 'z'; i >= 'a'; i--) {
         putchar(i);
-    }
     
     putchar('\n');
     
