69251d2e53a0b330f40028b6cb8c18e1bb6e7ba5
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-07-24 21:02:07 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_half - this is a function that prints half of a string, followed by a new line.
+ * puts_half - function that prints half of a string.
  * @str: An input string
  * Return: 0
  */
