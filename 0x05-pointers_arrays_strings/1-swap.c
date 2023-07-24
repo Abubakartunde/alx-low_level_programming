ee6b36be2621db3f512f2a7278f12b8f1ee780e0
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-07-24 18:56:04 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * s_int - is a function that swaps the values of two integers.
+ * swap_int - is a function that swaps the values of two integers.
  * @a: an inout integer pointer
  * @b: an input integer pointer
  * Return: 0
