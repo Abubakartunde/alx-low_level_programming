c114fd21a80efe629609f7e20cf9c6c1a28efa21
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-07-27 15:19:07 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _strncat - this is a function that concatenates two strings.
+ * @dest: this is an input string
+ * @src: this is an input string
+ * @n: this is an input integer
+ * Return: A pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
