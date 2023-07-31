a9448dd1df2b140b5c4079948c8153c99f287e10
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-07-31 15:33:47 +0100

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn -is a function that retrieve the length of a substring.
+ * @s: is an input string
+ * @accept: is an input character
+ * Return: returns pointer to c
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0, flag;
+	char *start = accept;
+
+	while (*s)
+	{
+		flag = 0;
+		while (*accept)
+		{
+			if (*accept == *s)
+			{
+				count++;
+				flag = 1;
+				break;
+			}
+			accept++;
+		}
+		s++;
+		accept = start;
+		if (flag == 0)
+			break;
+	}
+	return (count);
+}
