1bc8138725b474513b38d3cf2a55cc759a3c1d2f
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-08-28 05:14:43 +0100

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - A function that frees all elements in a linked list
+ * @head: A pointer to head element of list
+ * Return: empty
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
