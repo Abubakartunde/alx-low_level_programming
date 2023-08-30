05c724f49f9e644776a755bd22b2faf7933c7f5d
abubakar <99052288+Abubakartunde@users.noreply.github.com>
2023-08-30 08:14:40 +0100

    Create 103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * find_listint_loop - The function that finds the loop in a list
+ * @head: The pointer to listint_t structure
+ * Return: The address of the node where the loop start, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *one = head, *two = head;
+
+	while (one && two && two->next)
+	{
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
+		{
+			return (one);
+		}
+	}
+	return (NULL);
+}
